b9173eba049950b381468ef77c69ebbddab68370
pnegre <pere.negre@gmail.com>
2014-09-28 23:22:23 +0200

    Sembla que tira...

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 
 	(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'userauth/login.html'}),
+	(r'^loginexpert/$', 'django.contrib.auth.views.login', {'template_name': 'userauth/login2.html'}),
 	(r'^logout/$', 'userauth.views.logout'),
 
 	(r'^logingoogle2/$', 'userauth.views.logingoogle2'),
