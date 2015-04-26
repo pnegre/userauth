5b4243c70c42f6bc54675ab4fe74de603a3803fe
pnegre <pere.negre@gmail.com>
2015-04-26 09:04:22 +0200

    Eliminat clientlogin, ja deprecated

--- backend.py
+++ backend.py
@@ -15,6 +15,7 @@ import re, urllib2, urllib
 # que l'usuari és correcte
 class DummyBackend:
 	def authenticate(self, usernamemail=None):
+		print "Cridat authenticate DUMMYBACKEND"
 		try:
 			user = User.objects.get(username=usernamemail)
 		except User.DoesNotExist:
@@ -34,50 +35,51 @@ class DummyBackend:
 			return None
 
 
-# Realitzar el ClientLogin de google, a partir d'un user + password
-def checkEmail(email,password):
-	try:
-		if not re.match('.*\@esliceu.com',email):
-			return False
-		req = urllib2.urlopen('https://www.google.com/accounts/ClientLogin',urllib.urlencode({
-			'accountType': 'HOSTED',
-			'Email'      : email,
-			'Passwd'     : password,
-			'service'    : 'apps',
-		}))
-		return True
-	except:
-		return False
-
-
-
-# Backend per ClientLogin, de google. Per mitjans de 2015 ja estarà DEPRECATED
-# Per ara el mantenim per facilitar la vida als usuaris...
-class ClientLoginBackend:
-	def authenticate(self, username=None, password=None):
-		if not re.match('.*\@esliceu.com',username):
-			username = username + '@esliceu.com'
-
-		if not checkEmail(username,password):
-			return None
-
-		try:
-			user = User.objects.get(username=username)
-		except User.DoesNotExist:
-			user = User.objects.create_user(username,username,'')
-			user.set_unusable_password()
-			user.is_staff = False
-			user.is_superuser = False
-			user.save()
-
-		return user
-
-
-	def get_user(self, user_id):
-		try:
-			return User.objects.get(pk=user_id)
-		except User.DoesNotExist:
-			return None
+# # Realitzar el ClientLogin de google, a partir d'un user + password
+# def checkEmail(email,password):
+# 	try:
+# 		if not re.match('.*\@esliceu.com',email):
+# 			return False
+# 		req = urllib2.urlopen('https://www.google.com/accounts/ClientLogin',urllib.urlencode({
+# 			'accountType': 'HOSTED',
+# 			'Email'      : email,
+# 			'Passwd'     : password,
+# 			'service'    : 'apps',
+# 		}))
+# 		return True
+# 	except:
+# 		return False
+#
+#
+#
+# # Backend per ClientLogin, de google. Per mitjans de 2015 ja estarà DEPRECATED
+# # Per ara el mantenim per facilitar la vida als usuaris...
+# class ClientLoginBackend:
+# 	def authenticate(self, username=None, password=None):
+# 		print "Cridat authenticate CLIENTLOGIN"
+# 		if not re.match('.*\@esliceu.com',username):
+# 			username = username + '@esliceu.com'
+#
+# 		if not checkEmail(username,password):
+# 			return None
+#
+# 		try:
+# 			user = User.objects.get(username=username)
+# 		except User.DoesNotExist:
+# 			user = User.objects.create_user(username,username,'')
+# 			user.set_unusable_password()
+# 			user.is_staff = False
+# 			user.is_superuser = False
+# 			user.save()
+#
+# 		return user
+#
+#
+# 	def get_user(self, user_id):
+# 		try:
+# 			return User.objects.get(pk=user_id)
+# 		except User.DoesNotExist:
+# 			return None
 
 
 # Classe per limitar el nombre de logins, per evitar atacs de força bruta contra el servidor
@@ -93,6 +95,7 @@ class RateLimitMixin(object):
 	username_key = 'username'
 
 	def authenticate(self, **kwargs):
+		print "Cridat authenticate MODELBACKEND"
 		request = kwargs.pop('request', None)
 		username = kwargs[self.username_key]
 		# TODO: què passa quan fem login amb oauth2? El camp username està buit???? S'haurà de comprovar al servidor...
